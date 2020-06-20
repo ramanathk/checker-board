d6c625bb138a472f1c958f4508593b968c5f0781
ramanathk <65902429+ramanathk@users.noreply.github.com>
2020-06-20 19:50:26 +0530

    Update chker.py

--- chker.py
+++ chker.py
@@ -1,14 +1,11 @@
 import numpy as np
-
 import cv2
-
-n=int (25)
 b = np.ones([200,200],dtype = 'uint8')*255
-for i in range(n,200,n*2):
-        for j in range (n,200,n*2):
+for i in range(25,200,25*2):
+        for j in range (25,200,25*2):
 
-           b[j-n:j,i-n:i] = 0
-           b[j:j+n,i:i+n] = 0
+           b[j-25:j,i-25:i] = 0
+           b[j:j+25,i:i+25] = 0
 cv2.imshow(' 8*8',b)
 cv2.waitKey(0)
 cv2.destroyAllWindows()  
